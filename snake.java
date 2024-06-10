f91158d13f4ce5609eda073441bc149aa479c9ff
Becca <rebeka.borsikova@gmail.com>
2024-06-10 15:15:13 +0200

    Create snake.java

---
+++ snake.java
@@ -0,0 +1,13 @@
+package coding_in_java.Snake;
+
+import javax.swing.*;
+
+public class snake {
+ int boardWidth = 600;
+ int boardHeight = boardWidth;
+
+ JFrame frame = new JFrame("Snake");
+ frame.setVisible(true);
+ 
+
+}
